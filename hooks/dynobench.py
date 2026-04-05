7bfc673d9493afb6654534911424f3378c70b315
Hassam Sheikh <hassamsheikh1@gmail.com>
2026-04-04 21:28:09 -0400

    break: introduce syntax error for autofix testing

--- hooks/dynobench.py
+++ hooks/dynobench.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 import sys as _sys; _sys.path.insert(0, str(__import__("pathlib").Path(__file__).resolve().parent))
-
+import pithch pitch
 import argparse
 import json
 import os
